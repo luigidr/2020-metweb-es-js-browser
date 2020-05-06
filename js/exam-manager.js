58ebdb232ecee05d11e7e8751c9bb096a196061d
Luigi De Russis <luigidr@gmail.com>
2020-05-06 15:40:58 +0200

    add custom events for class communication

--- js/exam-manager.js
+++ js/exam-manager.js
@@ -11,10 +11,10 @@ class ExamManager {
      */
     buildMyExams() {
         this.exams.push(
-            new Exam(moment("2020-02-01", "YYYY-MM-DD"), "Algoritmi I", 9, "28"),
-            new Exam(moment("2020-02-06", "YYYY-MM-DD"), "Basi di dati e sistemi informativi", 6, "30L"),
-            new Exam(moment("2020-02-15", "YYYY-MM-DD"), "Paradigmi di programmazione", 9, "26"),
-            new Exam(moment("2019-09-10", "YYYY-MM-DD"), "Programmazione II", 9, "27")
+            new Exam(moment('2020-02-01', 'YYYY-MM-DD'), 'Algoritmi I', 9, '28'),
+            new Exam(moment('2020-02-06', 'YYYY-MM-DD'), 'Basi di dati e sistemi informativi', 6, '30L'),
+            new Exam(moment('2020-02-15', 'YYYY-MM-DD'), 'Paradigmi di programmazione', 9, '26'),
+            new Exam(moment('2019-09-10', 'YYYY-MM-DD'), 'Programmazione II', 9, '27')
         );
 
     }
